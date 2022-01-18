234215bdc910b8cbf20e6611ee2a507ba2c8e367
satriya.wicaksana888 <satriya.wicaksana888@gmail.com>
2022-01-18 12:33:37 +0700

    unit test for movie list

---
+++ app/src/test/java/com/example/notflix/ui/movie/MovieViewModelTest.kt
@@ -0,0 +1,23 @@
+package com.example.notflix.ui.movie
+
+import org.junit.Assert.*
+
+import org.junit.Before
+import org.junit.Test
+
+class MovieViewModelTest {
+
+    private lateinit var viewModel: MovieViewModel
+
+    @Before
+    fun setUp() {
+        viewModel = MovieViewModel()
+    }
+
+    @Test
+    fun getMovies() {
+        val movieEntities = viewModel.getMovies()
+        assertNotNull(movieEntities)
+        assertEquals(5, movieEntities.size)
+    }
+}
